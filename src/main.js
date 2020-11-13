ac699e484802360623876adad1cc58d3954fd820
Richard Campbell <ric@netrc.com>
2020-11-13 13:32:11 -0500

    epsilon

--- src/main.js
+++ src/main.js
@@ -9,17 +9,6 @@ import firebase from 'firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
-// { for webjournal
-  //apiKey: "AIzaSyDC31eu72cS5X-KahoOmORMqoXGZW7XGIg",
-  //authDomain: "webjournal.firebaseapp.com",
-  //databaseURL: "https://webjournal.firebaseio.com",
-  //projectId: "webjournal",
-  //storageBucket: "webjournal.appspot.com",
-  //messagingSenderId: "721905720562",
-  //appId: "1:721905720562:web:9e5f2fdd1690fa6d35446b",
-  //measurementId: "G-X3Y3HWYD1K"
-//};
-
 firebase.initializeApp( firebaseConfig )
 
 Vue.config.productionTip = false
