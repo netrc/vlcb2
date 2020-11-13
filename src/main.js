b668ce37b4d8bc55feeb209cbff18287a4141519
Richard Campbell <ric@netrc.com>
2020-11-13 13:23:13 -0500

    no key

--- src/main.js
+++ src/main.js
@@ -7,15 +7,6 @@ import router from './router'
 import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  projectId: "vlcbt1-bd686",
-  databaseURL: "https://vlcbt1-bd686.firebaseio.com",
-  apiKey: "AIzaSyDe7Rd8bznfvDRtCBP_iu6tmUyuzeCAZeg",
-  authDomain: "vlcbt1-bd686.firebaseapp.com",
-  storageBucket: "vlcbt1-bd686.appspot.com",
-  messagingSenderId: "88733113811",
-  appId: "1:88733113811:web:f380d388169eea7f206326"
-};
 
 
 // { for webjournal
