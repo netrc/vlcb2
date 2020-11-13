9698fae2cb41745fe05f934ae0bc0afbf7c81678
Richard Campbell <ric@netrc.com>
2020-11-13 13:14:43 -0500

    with vlcb2 configuration

--- src/main.js
+++ src/main.js
@@ -8,15 +8,26 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDC31eu72cS5X-KahoOmORMqoXGZW7XGIg",
-  authDomain: "webjournal.firebaseapp.com",
-  databaseURL: "https://webjournal.firebaseio.com",
-  projectId: "webjournal",
-  storageBucket: "webjournal.appspot.com",
-  messagingSenderId: "721905720562",
-  appId: "1:721905720562:web:9e5f2fdd1690fa6d35446b",
-  measurementId: "G-X3Y3HWYD1K"
+  projectId: "vlcbt1-bd686",
+  databaseURL: "https://vlcbt1-bd686.firebaseio.com",
+  apiKey: "AIzaSyDe7Rd8bznfvDRtCBP_iu6tmUyuzeCAZeg",
+  authDomain: "vlcbt1-bd686.firebaseapp.com",
+  storageBucket: "vlcbt1-bd686.appspot.com",
+  messagingSenderId: "88733113811",
+  appId: "1:88733113811:web:f380d388169eea7f206326"
 };
+
+
+// { for webjournal
+  //apiKey: "AIzaSyDC31eu72cS5X-KahoOmORMqoXGZW7XGIg",
+  //authDomain: "webjournal.firebaseapp.com",
+  //databaseURL: "https://webjournal.firebaseio.com",
+  //projectId: "webjournal",
+  //storageBucket: "webjournal.appspot.com",
+  //messagingSenderId: "721905720562",
+  //appId: "1:721905720562:web:9e5f2fdd1690fa6d35446b",
+  //measurementId: "G-X3Y3HWYD1K"
+//};
 
 firebase.initializeApp( firebaseConfig )
 
