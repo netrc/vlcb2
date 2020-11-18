dcae4a5da2296779d303c2794581efa3539296c9
Richard Campbell <ric@netrc.com>
2020-11-18 15:44:48 -0500

    md vierwer v0.0

---
+++ src/components/MdViewEdit.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <p> raw is:  {{raw}} </p>
+</div>
+</template>
+
+<script>
+//import navigation from "@/components/NavBar.vue";
+
+export default {
+    data() {
+        return {
+          raw: "just the raw text"
+        };
+    },
+    name: 'mdviewedit',
+    //components: {  // none
+    //    navigation
+    //},
+    //created() {
+    //  this.getAbout()
+    //},
+    methods: {
+        switchView() {
+            console.log(`switch view`)
+        }
+    }
+};
+</script>
